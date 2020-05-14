fab73f9321488d1f4849002e6de7adbd83a6d3ab
Rafael Schettino <rafael_tecnologia@outlook.com>
2020-05-14 19:27:29 -0300

    AE-639: inclusão de novos casos de teste

--- parser.js
+++ parser.js
@@ -1,12 +1,42 @@
-const blacklist = {
-    address: /(apto|bloco|apartamento|casa|bl\s\d+)/gi
+const blacklist = ({
+    address,
+    number,
+    complement,
+    neighborhood,
+    city,
+    state
+}) => {
+    if (/df/i.test(state)) {
+        return false;
+    }
+    if (/^(RUA|R)$/i.test(address)) {
+        return true;
+    }
+    return /(apto|bloco|apartamento|casa|ap|bl\s\d+)\b/gi.test(address);
 };
 
 const sanitizeAddress = (completeAddress) => {
     return completeAddress
-        .replace(/(.)-(.)/gi, '$1 - $2')
-        .replace(/\s\s+/gi, ' ');
+        .replace(/(,\-|\-,)/g, '-')
+        .replace(/(\d)\.(\d{2,})/g, '$1$2')
+        .replace(/\.\s/g, ' ')
+        .replace(/(.)([-,])/gi, '$1 $2')
+        .replace(/([-,])(.)/gi, '$1 $2')
+        .replace(/(\d+)([a-z]+)(\d+)/i, '$1 $2 $3')
+        .replace(/([a-z]+)(\d+)([a-z]+)/i, '$1 $2 $3')
+        .replace(/(\d+)([a-z]+)/i, '$1 $2')
+        .replace(/([a-z]+)(\d+)/i, '$1 $2')
+        .replace(/\s\s+/gi, ' ')
+        .replace(/\-\s\-/, '-');
 };
+
+const formatAddress = (parsedAddress) => {
+    return {
+        ...parsedAddress,
+        address: parsedAddress.address
+            .replace(/\s,\s/gi, ', ')
+    }
+}
 
 const parse = (completeAddress) => {
     const sanitizedAddress = sanitizeAddress(completeAddress);
@@ -14,18 +44,32 @@ const parse = (completeAddress) => {
     const patterns = [
         // ADDRESS - COMPLEMENT - NEIGHBORHOOD - CITY - DF
         /(?<address>.*)\s[-,]\s(?<complement>.*)\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>DF)/i,
+        // ADDRESS - COMPLEMENT - NEIGHBORHOOD - CITY - GO
+        /(?<address>.*(?<!\s))\s-\s(?<complement>.*)\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>GO)/i,
         // BR-999 - 10 - NEIGHBORHOOD - CITY - ST
-        /(?<address>^(\w{2})[\s-]\d+)\s[-,]\s(?<number>(\d+|S\/?N))\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i,
+        /(?<address>^(\w{2})[\s-]\d+)\s[-,]\s(((NR?|CASA|NUMERO)\s?)?(?<number>(\d+|S\/?N)))\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i,
         // ADDRESS - N99 COMPLEMENT - NEIGHBORHOOD - CITY - ST
-        /(?<address>.*(?<!\s))(\s[-,]\s|\s?,\s?)(((N|CASA\s)(?<number>\d+|S\/?N)))(\s-\s|\s)(?<complement>.*)\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i,
+        /(?<address>.*(?<!\s))(\s[-,]\s|\s?,\s?)((((NR?|CASA|NUMERO)\s?)(?<number>\d+|S\/?N)))(\s-\s|\s)(?<complement>.*)\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i,
         // ADDRESS - 99 - COMPLEMENT - NEIGHBORHOOD - CITY - ST
-        /(?<address>.*(?<!\s))(\s[-,]\s|\s?,\s?)(?<number>(\d+|S\/?N))(\s-\s|\s)(?<complement>.*)\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i,
+        /(?<address>.*(?<!\s))(\s[-,]\s|\s?,\s?)(((NR?|CASA|NUMERO)\s?)?(?<number>(\d+|S\/?N)))(\s-\s|\s)(?<complement>.*)\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i,
+        // ADDRESS - 99/COMPLEMENT - NEIGHBORHOOD - CITY - ST
+        /(?<address>.*(?<!\s))(\s[-,]\s|\s?,\s?)(((NR?|CASA|NUMERO)\s?)?(?<number>(\d+|S\/?N)))\s?\/\s?(?<complement>.*)\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i,
         // ADDRESS 99 COMPLEMENT - NEIGHBORHOOD - CITY - ST
-        /(?<address>.*(?<!\s))\s(?<number>(\d+|S\/?N))\s(-\s)?(?<complement>(CASA\s).*)\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i,
-        // ADDRESS 99 - COMPLEMENT - NEIGHBORHOOD - CITY - ST
-        /(?<address>.*(?<!\s))\s(?<number>(\d+|S\/?N))(\s-\s|\s)(?<complement>.*)\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i,
+        /(?<address>.*(?<!\s))\s(((NR?|CASA|NUMERO)\s?)?(?<number>(\d+|S\/?N)))\s(-\s)?(?<complement>((CASA|APTO|AP)\b\s).*)\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i,
+        // ADDRESS - 99 X - COMPLEMENT - NEIGHBORHOOD - CITY - ST
+        /(?<address>.*(?<!\s))\s(((NR?|CASA|NUMERO)\s?)?(?<number>(\d+|S\/?N)))\s(?<complement>\w\s-.*)\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i,
         // ADDRESS - 99 - NEIGHBORHOOD - CITY - ST
-        /(?<address>.*)\s[-,]\s(?<number>(\d+|S\/?N))\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i
+        /(?<address>.*(?<!\s))\s[-,]\s(((NR?|CASA|NUMERO)\s?)?(?<number>(\d+|S\/?N)))\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i,
+        // ADDRESS 99 - COMPLEMENT - NEIGHBORHOOD - CITY - ST
+        /(?<address>.*(?<!\s))\s(((NR?|CASA|NUMERO)\s?)?(?<number>(\d+|S\/?N)))\s-\s(?<complement>.*)\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i,
+        // ADDRESS 00 COMPLEMENT- COMPLEMENT2 99 - NEIGHBORHOOD - CITY - ST
+        /(?<address>.*(?<!\s))\s(((NR?|CASA|NUMERO)\s?)?(?<number>(\d+|S\/?N)))\s(?<complement>.*-.*)\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i,
+        // ADDRESS - ADDRESS2 99 - NEIGHBORHOOD - CITY - ST
+        /(?<address>.*(?<!\s))\s(((NR?|CASA|NUMERO)\s?)?(?<number>(\d+|S\/?N)))\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i,
+        // RURAL WITH COMPLEMENT
+        /(?<address>^(SITIO|FAZENDA|ESTRADA).*)\s[-,]\s(?<complement>.*)\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i,
+        // RURAL WITHOUT COMPLEMENT
+        /(?<address>^(SITIO|FAZENDA|ESTRADA).*)\s[-,]\s(?<neighborhood>.*)\s[-,]\s(?<city>.*)\s[-,]\s(?<state>\w{2})/i
     ];
     const pattern = patterns.find((p, index) => {
         const pat = p.test(sanitizedAddress);
@@ -40,20 +84,18 @@ const parse = (completeAddress) => {
     }
     const result = pattern.exec(sanitizedAddress);
     if (result && result.groups) {
-        const blacklisted = Object.keys(blacklist).some((key) => {
-            return blacklist[key].test(result.groups[key]);
-        });
+        const blacklisted = blacklist(result.groups);
         if (blacklisted) {
             return null;
         }
-        return {
+        return formatAddress({
             address: result.groups.address,
             number: (result.groups.number === undefined) ? '' : result.groups.number,
             complement: (result.groups.complement === undefined) ? '' : result.groups.complement,
             neighborhood: result.groups.neighborhood,
             city: result.groups.city,
             state: result.groups.state
-        };
+        });
     }
     return null;
 }
--- test/parser.test.js
+++ test/parser.test.js
@@ -7,8 +7,22 @@ const addresses = require('./addresses.json');
 suite('Parser', () => {
 
     test('Should sanitize address', () => {
-        const addressWithSpaces = 'RUA  DA  GROELANDIA    54-APTO 104 -BAIRRO';
-        const expected = 'RUA DA GROELANDIA 54 - APTO 104 - BAIRRO';
+        const addressWithSpaces = 'RUA  DA  GROELANDIA,    1.554B,-APTO. 104B03 -,BAIRRO';
+        const expected = 'RUA DA GROELANDIA , 1554 B - APTO 104 B 03 - BAIRRO';
+        const actual = parser.sanitizeAddress(addressWithSpaces);
+        assert.strictEqual(actual, expected);
+    });
+
+    test('Should sanitize address', () => {
+        const addressWithSpaces = 'R SILVIO ROMERO-8-AP 504 - SANTA TERESA - RIO DE JANEIRO - RJ';
+        const expected = 'R SILVIO ROMERO - 8 - AP 504 - SANTA TERESA - RIO DE JANEIRO - RJ';
+        const actual = parser.sanitizeAddress(addressWithSpaces);
+        assert.strictEqual(actual, expected);
+    });
+
+    test('Should sanitize with empty complement', () => {
+        const addressWithSpaces = 'RUA  DA  GROELANDIA,    1.554 -  - BAIRRO';
+        const expected = 'RUA DA GROELANDIA , 1554 - BAIRRO';
         const actual = parser.sanitizeAddress(addressWithSpaces);
         assert.strictEqual(actual, expected);
     })
